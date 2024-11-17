567733bf4d13d7d4c316c52e2d60fe2e0b0504f1
Srikant_kumar <srikant7roy@gmail.com>
2024-11-17 20:46:13 +0530

    third time pushing

--- javabasics/src/javabasics1/testing.java
+++ javabasics/src/javabasics1/testing.java
@@ -1,5 +1,16 @@
 package javabasics1;
 
-public class testing {
+public class testing
+{
+	public void testgit()
+	{
+		System.out.println("data is pusshed successfully");
+	}
+	public static void main(String[] args) 
+	{
+		testing te=new testing();
+		te.testgit();
+		
+	}
 
 }
