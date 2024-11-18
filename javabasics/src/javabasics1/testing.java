c2170e61dafcdada144bb1d10b80ffa2280fbf1e
Srikant_kumar <srikant7roy@gmail.com>
2024-11-18 17:35:55 +0530

    test

--- javabasics/src/javabasics1/testing.java
+++ javabasics/src/javabasics1/testing.java
@@ -2,14 +2,17 @@ package javabasics1;
 
 public class testing
 {
-	public void testgit()
+	public void testgit(int a, int b, int c)
 	{
-		System.out.println("data is pusshed successfully");
+		int d;
+		d=a+b-c;
+		System.out.println("Value of d="+d);
 	}
 	public static void main(String[] args) 
 	{
-		testing te=new testing();
-		te.testgit();
+		testing ttt=new testing();
+		ttt.testgit(100, 500, 20);
+		System.out.println("Hello i am main method");
 		
 	}
 
