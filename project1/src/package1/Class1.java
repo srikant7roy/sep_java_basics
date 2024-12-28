95e837fe2ecbc140855190d58d5aebc903b63dd9
Srikant_kumar <srikant7roy@gmail.com>
2024-12-28 13:31:39 +0530

    project1

---
+++ project1/src/package1/Class1.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class Class1 
+{
+	public void m1()
+	{
+		System.out.println("Project1 class");
+	}
+	public void m2()
+	{
+		System.out.println("projcet1 class");
+	}
+	public void m3()
+	{
+		System.out.println("Hey i am sirkant");
+	}
+}
